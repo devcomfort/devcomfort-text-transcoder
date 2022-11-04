c710624d069e973da7ab92a5c3afc5c677499749
devcomfort <kimdonghyun026@gmail.com>
2022-11-04 21:18:07 +0900

    JSDoc 추가

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,12 @@
 import { encode, decode } from "iconv-lite";
 
+/**
+ * `start` 형식 인코딩에서 `end` 형식 인코딩으로 변환하는 함수
+ * @param {string} str
+ * @param {string} start
+ * @param {string} end
+ * @returns
+ */
 const reEncode = (str, start, end) => decode(encode(str, start), end);
 
 export default reEncode;
