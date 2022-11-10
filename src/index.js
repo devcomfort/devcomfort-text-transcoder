74426c452143ca69d94f2c2b9b0e59058caa0385
devcomfort <kimdonghyun026@gmail.com>
2022-11-10 15:51:23 +0900

    의존성 및 구조 변경 (CommonJS만 지원하도록 package.json 변경)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,4 @@ const { encode, decode } = require("iconv-lite");
  */
 const reEncode = (str, start, end) => decode(encode(str, start), end);
 
-module.exports = {
-  reEncode,
-};
+module.exports = { reEncode };
