0987798ccd120333adefda78162c627236dd46f2
devcomfort <kimdonghyun026@gmail.com>
2022-11-13 01:42:37 +0900

    타입 힌트 추가

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@
 
 import { TextDecoder, TextEncoder } from "text-decoding";
 
+/**
+ *
+ * @param {string} str 문자열
+ * @param {string} start 시작 인코딩
+ * @param {string} end 끝 인코딩
+ * @returns {string}
+ */
 const reEncode = (str, start, end) =>
   new TextDecoder(end).decode(
     new TextEncoder(start, {
