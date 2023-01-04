918bec172d5b1585b99e1eefb0998d20a34202a6
devcomfort <kimdonghyun026@gmail.com>
2023-01-04 15:10:23 +0900

    데드코드 제거

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// const { encode, decode } = require("iconv-lite");
-
-// /**
-//  * `start` 형식 인코딩에서 `end` 형식 인코딩으로 변환하는 함수
-//  * @param {string} str
-//  * @param {string} start
-//  * @param {string} end
-//  * @returns
-//  */
-// const reEncode = (str, start, end) => decode(encode(str, start), end);
-
-// module.exports = { reEncode };
-
 import { TextDecoder, TextEncoder } from "text-decoding";
 
 /**
